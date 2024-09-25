1253878ae9b8662a4be564e6bac5e3ccc4410d62
SHADOWArab <87262605+SHADOWArab@users.noreply.github.com>
2024-09-26 02:07:43 +0530

    added routes,ejs and css

--- app.js
+++ app.js
@@ -6,29 +6,37 @@ const homeContent  = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. N
 const contactContent ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc faucibus dignissim tristique. Vestibulum varius erat elit, in condimentum turpis tempor sed. Maecenas felis nisl, lacinia id felis vitae, bibendum laoreet nisi. Suspendisse justo justo, consectetur quis sapien sit amet, tempor hendrerit nisi. Ut ultrices fringilla auctor. Aliquam erat volutpat. Proin in tristique nisi.";
 const aboutContent =  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc faucibus dignissim tristique. Vestibulum varius erat elit, in condimentum turpis tempor sed. Maecenas felis nisl, lacinia id felis vitae, bibendum laoreet nisi. Suspendisse justo justo, consectetur quis sapien sit amet, tempor hendrerit nisi. Ut ultrices fringilla auctor. Aliquam erat volutpat. Proin in tristique nisi.";
 
+var posts =[];
+
+
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+
+
 app.get("/", function (req, res){    
-    res.render("home", {homeContent: homeContent});
+    res.render("home", {startingContent: homeContent , posts : posts });
+
 });
-
-
 app.get("/about", function (req, res){   
     res.render("about", {aboutContent:aboutContent});
 });
-
-
-
 app.get("/contact", function (req, res){   
-    res.render(contactContent);
-    
+    res.render("contact", {contactContent:contactContent});
+});
+app.get("/compose", function (req, res){   
+    res.render("compose");
 });
 
-app.get("/footer", function(req,res){
-    res.render("footer");
+app.post("/compose", function(req,res){
+   const post = {
+       postTitle : req.body.postTitle,
+       postContent : req.body.postContent 
+   }
+   posts.push(post);
+   res.redirect("/");
 });
 
 app.listen(3000, function(){
